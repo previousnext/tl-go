9f024ce106292098f485c468134aa3f673bf54f2
Kim Pepper <kim@pepper.id.au>
2026-02-17 12:35:20 +1100

    Add test for nil category

--- cmd/list/command_test.go
+++ cmd/list/command_test.go
@@ -63,3 +63,50 @@ func TestNewCommand_PrintsEntriesInTable(t *testing.T) {
 	assert.Contains(t, output, "2h0m")
 	assert.Contains(t, output, "30m")
 }
+
+func TestNewCommand_PrintsEntriesWithNilCategory(t *testing.T) {
+	mock := &mocks.MockRepository{
+		FindAllTimeEntriesFunc: func(date time.Time) ([]*model.TimeEntry, error) {
+			project1 := model.Project{Name: "Project1", Category: nil}
+			project2 := model.Project{Name: "Project2", Category: nil}
+			return []*model.TimeEntry{
+				{
+					Model:    gorm.Model{ID: 1},
+					IssueKey: "PNX-1",
+					Issue: &model.Issue{
+						Summary: "issue1",
+						Project: project1,
+					},
+					Duration:    2 * time.Hour,
+					Description: "Worked on X",
+				},
+				{
+					Model:    gorm.Model{ID: 2},
+					IssueKey: "PNX-2",
+					Issue: &model.Issue{
+						Summary: "issue2",
+						Project: project2,
+					},
+					Duration:    30 * time.Minute,
+					Description: "Reviewed Y",
+				},
+			}, nil
+		},
+	}
+	cmd := NewCommand(func() db.TimeEntriesInterface { return mock })
+
+	var buf bytes.Buffer
+	cmd.SetOut(&buf)
+
+	err := cmd.Execute()
+	assert.NoError(t, err)
+	output := buf.String()
+	assert.Contains(t, output, "PNX-1")
+	assert.Contains(t, output, "PNX-2")
+	assert.Contains(t, output, "issue1")
+	assert.Contains(t, output, "issue2")
+	assert.Contains(t, output, "Worked on X")
+	assert.Contains(t, output, "Reviewed Y")
+	assert.Contains(t, output, "2h0m")
+	assert.Contains(t, output, "30m")
+}
